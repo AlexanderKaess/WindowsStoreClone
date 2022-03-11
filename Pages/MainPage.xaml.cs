21e8b38d378931b0167e09a8f42b93cc81ac114b
Alexander Kaess <alexander.kaess@zeiss.com>
2022-03-11 11:16:07 +0100

    added animation property opacity in Mainpage

--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -28,6 +29,23 @@ namespace WindowsStoreClone.Pages
         private void FeaturedAppsViewer_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void MainScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+            UIElement element = (UIElement)sender;
+            element.Opacity = 0;
+
+            //set the animation of opacity from white to no opacity within 10 seconds
+            DoubleAnimation animation = new DoubleAnimation()
+            {
+                From = 0,
+                To = 1,
+                Duration = new Duration(new TimeSpan(0, 0, 5))
+            };
+
+            //begin the animation, Opacity.Property is the property (target) which we want to change and with the animation we want
+            element.BeginAnimation(UIElement.OpacityProperty, animation);
         }
     }
 }
